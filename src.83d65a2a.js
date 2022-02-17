209a5f48a6b738723e278f0aa722d458b36fa219
Artem-Borzov <95220251+Artem-Borzov@users.noreply.github.com>
2022-02-17 17:18:26 +0000

    Deploying to gh-pages from @ Artem-Borzov/FSS-icecream@b7dd4557af6904a1c69479bf833c3d1518356c5e 🚀

--- src.83d65a2a.js
+++ src.83d65a2a.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/FSS-icecream/src.693e5678.js.map
+//# sourceMappingURL=/FSS-icecream/src.83d65a2a.js.map
